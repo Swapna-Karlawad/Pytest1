6de986fdc1b8cd548ba41a2aef54cddc7c6dd1db
Swapna-Karlawad <swapnakarlawad@gmail.com>
2026-01-15 10:59:16 +0530

    save

---
+++ Temprature.py
@@ -0,0 +1,14 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+
+def main():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+
+    print(f"Celsius: {celsius}")
+    print(f"Fahrenheit: {fahrenheit}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Test_Temprature.py
@@ -0,0 +1,6 @@
+from Temprature import celsius_to_fahrenheit
+
+def test_zero():
+    assert celsius_to_fahrenheit(0) == 32
+
+
